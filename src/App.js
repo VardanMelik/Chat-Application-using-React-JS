2eedb19cada645154d5502e03b906c77e2ce9d65
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-03-18 15:09:03 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import { ChatEngine, ChatFeed } from 'react-chat-engine'
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 
 function App() {
+
+  if (!localStorage.getItem('username'))
+    return <LoginForm/>
+
   return (
     <div className="App">
       <ChatEngine
         height="100vh"
         publicKey="43f00b2d-93bf-47c6-84f6-c1964cdeab51"
-        userName="React-Chat-Admin"
-        userSecret="2H5MLUadrJ5nH8fL"
-        renderChatFeed={ (chatAppProps) => <ChatFeed {...chatAppProps} /> }
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={ (chatAppProps) => 
+            <ChatFeed {...chatAppProps} /> 
+          }
       />
     </div>
   );
--- timer.js
+++
@@ -1,4 +0,0 @@
-/*
-14:18/1:10:07
-https://www.youtube.com/watch?v=jcOKU9f86XE&ab_channel=JavaScriptMastery
-*/
